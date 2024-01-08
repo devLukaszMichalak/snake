2772c702cd08688434dcf62c8b791f89c9fec53a
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2024-01-08 21:27:19 +0100

    refactor hammer config

--- src/app/hammer-config.ts
+++ src/app/hammer-config.ts
@@ -1,9 +1,7 @@
 import { HammerGestureConfig } from '@angular/platform-browser';
 
-const DIRECTION_ALL = (window as any).Hammer.DIRECTION_ALL;
-
 export class HammerConfig extends HammerGestureConfig {
   override overrides = <any>{
-    swipe: {direction: DIRECTION_ALL}
+    swipe: {direction: Hammer.DIRECTION_ALL}
   };
 }
